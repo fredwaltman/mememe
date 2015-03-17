541b45ad31ee994bcb8e5e198564e758f9c8dcdc
Fred Waltman <fwaltman@gmail.com>
2015-03-17 15:41:37 -0700

    Committed by Udacity

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Fred Waltman on 3/11/15.
+//  Copyright (c) 2015 Fred Waltman. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImage: UIImageView!
+}
