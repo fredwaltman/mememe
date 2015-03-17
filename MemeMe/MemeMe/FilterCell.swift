af01c741ed2fe522c8edc42a230ed2438ee5f5dc
Fred Waltman <fwaltman@gmail.com>
2015-03-17 15:41:38 -0700

    Committed by Udacity

---
+++ MemeMe/MemeMe/FilterCell.swift
@@ -0,0 +1,17 @@
+//
+//  FilterCell.swift
+//  MemeMe
+//
+//  Created by Fred Waltman on 3/17/15.
+//  Copyright (c) 2015 Fred Waltman. All rights reserved.
+//
+
+import UIKit
+
+class FilterCell: UICollectionViewCell {
+    
+     @IBOutlet weak var cellImage: UIImageView!
+ 
+    @IBOutlet weak var cellLabel : UILabel!
+}
+
