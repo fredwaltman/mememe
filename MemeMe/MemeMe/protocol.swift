defc4912e60fc49037644d56b82ada3045540156
Fred Waltman <fwaltman@gmail.com>
2015-03-17 15:41:46 -0700

    Committed by Udacity

---
+++ MemeMe/MemeMe/protocol.swift
@@ -0,0 +1,14 @@
+//
+//  protocol.swift
+//  MemeMe
+//
+//  Created by Fred Waltman on 3/17/15.
+//  Copyright (c) 2015 Fred Waltman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol SelectFilterDelegate {
+    func didSelectFilterImage(image : UIImage)
+}
